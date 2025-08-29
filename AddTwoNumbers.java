02d2cf43314f5ad301d58e3fe8fda3681edffae5
poopathynathan <poopathynathan2005@gmail.com>
2025-08-29 12:40:25 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,22 +1,9 @@
-import java.util.Scanner;
-
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int a = 10;  // first number
+        int b = 20;  // second number
+        int sum = a + b;  // addition
 
-        // Input numbers
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
-
-        // Calculate sum
-        int sum = num1 + num2;
-
-        // Display result
-        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
-        
-        sc.close();
+        System.out.println("Sum = " + sum);
     }
 }
